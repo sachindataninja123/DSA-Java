c5e162a3d1c3e00124748ab720e0bd4f15f743cd
Sachin Kushwaha <sachinkumarkushwaha991071@gmail.com>
2026-09-01 00:06:41 +0530

    triangle pattern complete

---
+++ src/Pattern_printing/alphaNumericTriangle.java
@@ -0,0 +1,24 @@
+package Pattern_printing;
+
+import java.util.Scanner;
+
+public class alphaNumericTriangle {
+    static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        for(int i = 1; i <= n; i++){
+            for(int j = 1; j<=i; j++){
+                if(i % 2 == 0){
+                    System.out.print((char)(j+64) + " ");
+                }
+                else {
+                    System.out.print(j + " ");
+                }
+            }
+
+            System.out.println();
+        }
+    }
+}
---
+++ src/Pattern_printing/numberTriangle.java
@@ -0,0 +1,19 @@
+package Pattern_printing;
+
+import java.util.Scanner;
+
+public class numberTriangle {
+    static void main(String[] args) {
+        Scanner sc  = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        for(int i = 1; i<=n; i++ ){
+            for(int j = 1; j <= i; j++){
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+    }
+    }
+
